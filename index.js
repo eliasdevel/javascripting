44b6b491a2e6485133056b2f37c85b88e1f1e8ae
Elias Müller <elias.muller1@gmail.com>
2019-12-17 12:53:19 -0200

    ajust

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/main.js', function (req, res) {
-    res.sendfile(__dirname + '/main.js');
+    res.sendFile(__dirname + '/main.js');
 });
 app.get('/index.js', function (req, res) {
-    res.sendfile(__dirname + '/index.js');
+    res.sendFile(__dirname + '/index.js');
 });
 app.get('/Xhr.js', function (req, res) {
-    res.sendfile(__dirname + '/Xhr.js');
+    res.sendFile(__dirname + '/Xhr.js');
 });
 
 app.get('/tsconfig.json', function (req, res) {
-    res.sendfile(__dirname + '/tsconfig.json');
+    res.sendFile(__dirname + '/tsconfig.json');
 });
 
 app.listen(3000, function () {
